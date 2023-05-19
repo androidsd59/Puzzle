0076ac439b21235180ce332f4ea8f660fbb2b807
androidsd59 <dragoev@mksat.net>
2023-05-19 18:26:42 +0300

    Change peremeters project v.2.0.5

--- app/src/main/java/cn/kevin/puzzle/PuzzleActivity.java
+++ app/src/main/java/cn/kevin/puzzle/PuzzleActivity.java
@@ -1,14 +1,15 @@
 package cn.kevin.puzzle;
 
 import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class PuzzleActivity extends AppCompatActivity implements Runnable, View.OnTouchListener {
     PuzzleLayout puzzleLayout;
